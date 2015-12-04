34e1c89fe63759473caccccb2df7405f49829a94
Anatoliy Kats <anatoliy314@gmail.com>
2015-12-03 20:38:49 -0500

    Some models from tutorial

--- src/snowranger/snowranger/settings.py
+++ src/snowranger/snowranger/settings.py
@@ -75,14 +75,13 @@ WSGI_APPLICATION = 'snowranger.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+         'NAME': 'snowranger',
+         'USER': 'anatoliy',
+     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
