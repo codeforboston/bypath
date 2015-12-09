f2e459d19c1ce9cef2013ae68bfd5b4a43b797ec
Anatoliy Kats <anatoliy314@gmail.com>
2015-12-08 21:24:16 -0500

    Google maps API hello world

--- src/snowranger/conditions/views.py
+++ src/snowranger/conditions/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
+
+def map(request):
+    return render_to_response('conditions/SnowRanger.html')
