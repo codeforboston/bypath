d9c7b07867c329e52aa3875b3dd0631b1d99c8d1
Anatoliy Kats <anatoliy314@gmail.com>
2015-12-12 14:23:06 -0500

    Forgot urls.py for hello world

--- src/snowranger/conditions/views.py
+++ src/snowranger/conditions/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render_to_response
 # Create your views here.
 
 def map(request):
-    return render_to_response('conditions/SnowRanger.html')
+    return render_to_response('conditions/snowranger.html')
