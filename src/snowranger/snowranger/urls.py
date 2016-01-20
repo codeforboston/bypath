95c6c4e311431602169bf57e212ce7ad81e39fa6
Anatoliy Kats <anatoliy314@gmail.com>
2016-01-19 20:29:14 -0500

    display website from root URL

--- src/snowranger/snowranger/urls.py
+++ src/snowranger/snowranger/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from conditions import views
 
 urlpatterns = [
+    url(r'^$', views.map),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^conditions/', include('conditions.urls', namespace="conditions")),
 ]
