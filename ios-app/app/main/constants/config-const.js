7b79ec11969594a0ce217af663e1b8b5eff1d42c
Mr. Is <isaac.ardis@gmail.com>
2016-03-01 20:34:46 -0500

    init ios from m-ionic

---
+++ ios-app/app/main/constants/config-const.js
@@ -0,0 +1,19 @@
+'use strict';
+angular.module('main')
+.constant('Config', {
+
+  // gulp environment: injects environment vars
+  // https://github.com/mwaylabs/generator-m-ionic#gulp-environment
+  ENV: {
+    /*inject-env*/
+    /*endinject*/
+  },
+
+  // gulp build-vars: injects build vars
+  // https://github.com/mwaylabs/generator-m-ionic#gulp-build-vars
+  BUILD: {
+    /*inject-build*/
+    /*endinject*/
+  }
+
+});
---
+++ ios-app/app/main/main.js
@@ -0,0 +1,46 @@
+'use strict';
+angular.module('main', [
+  'ionic',
+  'ngCordova',
+  'ui.router',
+  // TODO: load other modules selected during generation
+])
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  // ROUTING with ui.router
+  $urlRouterProvider.otherwise('/main/list');
+  $stateProvider
+    // this state is placed in the <ion-nav-view> in the index.html
+    .state('main', {
+      url: '/main',
+      abstract: true,
+      templateUrl: 'main/templates/tabs.html'
+    })
+      .state('main.list', {
+        url: '/list',
+        views: {
+          'tab-list': {
+            templateUrl: 'main/templates/list.html',
+            // controller: 'SomeCtrl as ctrl'
+          }
+        }
+      })
+      .state('main.listDetail', {
+        url: '/list/detail',
+        views: {
+          'tab-list': {
+            templateUrl: 'main/templates/list-detail.html',
+            // controller: 'SomeCtrl as ctrl'
+          }
+        }
+      })
+      .state('main.debug', {
+        url: '/debug',
+        views: {
+          'tab-debug': {
+            templateUrl: 'main/templates/debug.html',
+            controller: 'DebugCtrl as ctrl'
+          }
+        }
+      });
+});
