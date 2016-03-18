7a45a525cb098de668cb962c04593b436f3e3c06
Alex <andermerritt@gmail.com>
2016-03-18 14:59:16 -0400

    Started working on the back end
    This is a restful server written in node.js
    This can currently add an incident from a web form to the firebase.

---
+++ back-end/SnowrangerNode/README.md
@@ -0,0 +1,5 @@
+﻿This project is for the back end server for the snowranger project
+
+This project was created using visual studio but it should run on any os with the proper node.js instalation.
+
+All of the dependencies are located in the package.json file.
---
+++ back-end/SnowrangerNode/database/db_firebase.js
@@ -0,0 +1,57 @@
+﻿var firebase = require('firebase');
+
+var firebase_url = "https://alexdev.firebaseio.com/";
+var fbRef = new firebase(firebase_url);
+
+var MASTER = 'MASTER';
+var VALUE = 'VALUE'
+var PATH = 'PATH';
+var DATA = 'DATA';
+
+function generateSchema(data) {
+    // Grab change the data's format to fit the db
+    // in the form of <path>: <values>
+    var values = [];
+    //output['/master'] = { 'id': data['id'] };
+    values.push(createSchemaItem('/open', data['open']));
+    values.push(createSchemaItem('/type', data['type']));
+    values.push(createSchemaItem('/title', data['title']));
+    values.push(createSchemaItem('/location', data['loc']));
+    values.push(createSchemaItem('/geo', data['geo']));
+    
+    var output = {
+        MASTER: createSchemaItem('/master', { 'id': data['id'] }),
+        VALUE : values
+    }
+    
+    return output;
+}
+
+function createSchemaItem(path, item) {
+    return { PATH : path, DATA : item };
+}
+
+module.exports = {
+    addItem: function (data){
+        // generate the schema from the data passed in
+
+        var result = generateSchema(data);
+        //console.log(result);
+        
+        // Get the master
+        master = result[MASTER];
+        
+        var response = fbRef.child(master[PATH]).push(master[DATA]);
+        
+        console.log(response.path.u[1]);
+
+        var values = result[VALUE];
+        for (i in values) {
+            var item = values[i];
+            fbRef.child(item[PATH] + '/' + response.path.u[1]).set(item[DATA]);
+        }
+        
+        
+    }
+}
+
