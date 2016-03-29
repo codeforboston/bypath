5067913f4f4b50c26c021b426bba74ea37f1143c
Andrew Lawson <aeriklawson@gmail.com>
2016-03-29 19:02:44 -0400

    Removed old Django app. Moved chatbot to project root.

--- src/chatbot/FacebookEndpoint.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-# I need to figure out how I want to deal with these classes
-
-class FacebookEndpoint:
-    pass
--- src/snowranger/conditions/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render_to_response
-
-# Create your views here.
-
-def map(request):
-    return render_to_response('conditions/snowranger.html')
