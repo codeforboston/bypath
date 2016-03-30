7f3c332e0aee092dfb86d94c167591b4b01b785b
Alex Merritt <andermerritt@gmail.com>
2016-03-29 20:23:51 -0400

    fixed port of server

--- rest/rest_server.js
+++ rest/rest_server.js
@@ -11,6 +11,7 @@ var incident = require('./routes/incident.js');
 
 // private memeber vars
 var app;
+var port;
 
 // public methods
 module.exports = {
@@ -22,10 +23,17 @@ module.exports = {
     },
 
     start: function (){
+        port = process.env.PORT || 8080;
         console.log('rest server module started');
-        var server = app.listen(8080, function () {
+        var server = app.listen(port, function(){
+            console.log('Server started on port: ' + port);
         });
     }
 }
 
 // private methods
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var port = process.env.PORT || 8080;
+connect().use(serveStatic(__dirname)).listen(port);
