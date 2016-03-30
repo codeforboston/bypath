0490f1175574139745c8c49ff5c9bc7f73e0e462
Alex Merritt <andermerritt@gmail.com>
2016-03-29 20:26:42 -0400

    fixed the ooops

--- rest/rest_server.js
+++ rest/rest_server.js
@@ -32,8 +32,3 @@ module.exports = {
 }
 
 // private methods
-
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var port = process.env.PORT || 8080;
-connect().use(serveStatic(__dirname)).listen(port);
