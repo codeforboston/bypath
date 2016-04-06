6b832b88b0264634f0138f534696d7364e6f8f62
Mr.ia <rrysteryfactory@gmail.com>
2016-04-06 08:57:08 -0400

    filterable by all unique case types

--- ios/app/main/filters/three-one-one-by-id-fil.js
+++ ios/app/main/filters/three-one-one-by-id-fil.js
@@ -6,6 +6,51 @@ angular.module('main')
   };
 })
 
+// .filter('unique', function() {
+//     return function (arr, field) {
+//         return _.uniq(arr, function(a) { return a[field]; });
+//     };
+// })
+//
+
+.filter('unique', function () {
+
+  return function (items, filterOn) {
+
+    if (filterOn === false) {
+      return items;
+    }
+
+    if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+      var hashCheck = {}, newItems = [];
+
+      var extractValueToCompare = function (item) {
+        if (angular.isObject(item) && angular.isString(filterOn)) {
+          return item[filterOn];
+        } else {
+          return item;
+        }
+      };
+
+      angular.forEach(items, function (item) {
+        var valueToCheck, isDuplicate = false;
+
+        for (var i = 0; i < newItems.length; i++) {
+          if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+            isDuplicate = true;
+            break;
+          }
+        }
+        if (!isDuplicate) {
+          newItems.push(item);
+        }
+
+      });
+      items = newItems;
+    }
+    return items;
+  };
+})
 
 /**
  * Return only cases that match the given case types.
