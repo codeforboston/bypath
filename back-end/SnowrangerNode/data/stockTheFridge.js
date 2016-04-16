f979ebf027665a17a83fdbfcb27ae20e3791e738
Mr.ia <rrysteryfactory@gmail.com>
2016-04-15 21:57:13 -0400

    export top shelf array for a given city

--- back-end/SnowrangerNode/data/stockTheFridge.js
+++ back-end/SnowrangerNode/data/stockTheFridge.js
@@ -1,6 +1,6 @@
 var fs = require('fs-extra-promise'); // for writing and reading files
 var util = require('util'); // for inspecting loggables
-var request = require('sync-request');
+var request = require('sync-request'); // yea, n sync. yea, yea, yea. 
 var _ = require('underscore');
 
 var key = '';
@@ -27,7 +27,7 @@ function getCitiesData() {
 		var daUrl = val['url'].replace('$query=','') + '$limit=' + limiter.toString() + '&$group=' + daType + '&$select=' + daType + ',count(' + daType + ')' + '&$order=' + 'count_' + daType + ' DESC';// + '&$order=';
 		var daOpts = {
 			'headers': {
-	        'X-App-Token': 'zdkQROnSL8UlsDCjuiBcc3VHq' // key
+	        'X-App-Token': 'zdkQROnSL8UlsDCjuiBcc3VHq' // key <- keys.txt one was throwing a permission denied error
 	    }
 	  };
 		var res = request(method, daUrl, daOpts);
@@ -43,10 +43,38 @@ function handleError(err) {
 	console.log(util.inspect(err));
 }
 
-getKey()
-.then(setKey)
-.then(getCities)
-.then(setCities)
-.then(getCitiesData)
-.catch(handleError);
+exports.fridayNight = function() {
+	getKey()
+	.then(setKey)
+	.then(getCities)
+	.then(setCities)
+	.then(getCitiesData)
+	.catch(handleError);
+};
 
+/**
+ * Gets array of top case types. DOES NOT update the data. Just formats it nicely.
+ * @param  {string} endPointKey as per endpoints.json
+ * @return {array.<string>}             ... which could be tossed into getMeASoda. 
+ */
+// exports.topTypes = function(endPointKey) {
+exports.topShelf = function(endPointKey) {
+	var r = [];
+	return getCities()
+		.then(setCities)
+		.then(function() {
+			fs.readFileAsync('./refrig.json', 'utf8').then(function(data) {
+				var data = JSON.parse(data);
+				var cityData = JSON.parse(data[endPointKey]); // array
+				var typeName = cities[endPointKey]['map']['type'];
+				for (var i = 0; i < cityData.length; i++) { // could use underscore
+					r.push(cityData[i][typeName]);
+				}
+				console.log(util.inspect(r));
+				return r;
+			});
+		})
+		.catch(handleError);
+}
+
+// topTypes('boston_311');
