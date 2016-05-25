d1881afaee8b14e44ffeb8490ade23c15cccd217
Andrew Lawson <aeriklawson@gmail.com>
2016-05-25 12:31:00 -0400

    Reformat main.js and app.js. Rename 'mappy' to 'map' across controller, template, and config. Add .eslintrc file.

--- front-end/app/app.js
+++ front-end/app/app.js
@@ -4,8 +4,6 @@ angular.module('ByPath', [
   'main'
 ]);
 
-// Leaflet map puts tons of junk in the console window.
-// You have to disable debug logging if you want to disable it
 angular.module('ByPath').config(['$logProvider',
     function($logProvider) {
         $logProvider.debugEnabled(false);
--- front-end/app/main/main.js
+++ front-end/app/main/main.js
@@ -10,127 +10,80 @@ angular.module('main', [
   'firebase',
   'firebase.config',
   'leaflet-directive'
-  // TODO: load other modules selected during generation
 ])
-// for the camera
-// http://learn.ionicframework.com/formulas/cordova-camera/
-.config(function($compileProvider){
-  $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel|data):/); // data is safe!
-})
 .config(function ($stateProvider, $urlRouterProvider) {
-
-  // ROUTING with ui.router
-  $urlRouterProvider.otherwise('/main/mappy');
-  $stateProvider
+    $urlRouterProvider.otherwise('/main/map');
+    $stateProvider
     // this state is placed in the <ion-nav-view> in the index.html
     .state('main', {
-      url: '/main',
-      abstract: true,
-      templateUrl: 'main/templates/tabs.html',
-      controller: 'MainCtrl as mainCtrl',
-      // Resolve sets these vars as a dependency for the view and controller load, allowing
-      // the vars to be injected into the controller just like a factory.
-      resolve: {
-        /**
-         * @GeoLocation: GeoLocation factory
-         * Returns -->
-         *   // here: {
-             //   location: {
-             //     coords: {
-               // altitude: null
-               // altitudeAccuracy: null
-               // heading: null
-               // latitude: 42.40128642893472
-               // longitude: -71.12408442000427
-               // speed: null
-             //   },
-             //   address: '47 paulina st stomervilel'
-             // }
-         */
-        here: function (Geolocation) {
-          return Geolocation.get().then(function(loc) {
-            return Geolocation.getNearByCity(loc.coords.latitude, loc.coords.longitude).then(function (add) {
-              return {location: loc, address: add.data.results[0]['formatted_address']};
-            });
-          });
-        },
-        /**
-         * @Database: Database factory
-         * return factory assembled objects based on getObjectAll() helper method
-         */
-        toos: function (Database) {
-          return Database.getObjectAll().then(function(data) {
-            return Database.assembleObjects(data);
-          });
-        },
-        opinions: function (Opinions) {
-          return Opinions.index();
+        url: '/main',
+        abstract: true,
+        templateUrl: 'main/templates/tabs.html',
+        controller: 'MainCtrl as mainCtrl',
+        // Resolve sets these vars as a dependency for the view and controller load, allowing
+        // the vars to be injected into the controller just like a factory.
+        resolve: {
+            /**
+             * @GeoLocation: GeoLocation factory
+             * Returns --> here()
+            */
+            here: function(Geolocation) {
+                return Geolocation.get()
+                .then(function(location) {
+                    // Retrieve lat / long pair from get() function.
+                    var latitude = location.coords.latitude;
+                    var longitude = location.coords.longitude;
+                    return Geolocation.getNearByCity(latitude, longitude)
+                    .then(function(address) {
+                        // Retrieve formatted address from getNearByCity() function.
+                        var formatted_address = address.data.results[0]['formatted_address']
+                        return {
+                            location: loc,
+                            address: formatted_address
+                        };
+                    });
+                });
+            },
+            /**
+            * @Database: Database factory
+            * Return factory assembled objects based on getObjectAll() helper method
+            */
+            toos: function(Database) {
+                return Database.getObjectAll()
+                .then(function(data) {
+                    return Database.assembleObjects(data);
+                });
+            },
+            /**
+            * @Opinions: Opinions service
+            * Return factory assembled objects based on getObjectAll() helper method
+            */
+            opinions: function(Opinions) {
+                return Opinions.index();
+            }
         }
-      }
     })
-      // .state('main.debug', {
-      //   url: '/debug',
-      //   views: {
-      //     'tab-debug': {
-      //       templateUrl: 'main/templates/debug.html',
-      //       controller: 'DebugCtrl'
-      //     }
-      //   }
-      // })
-      .state('main.mappy', {
-        url: '/mappy',
+    .state('main.map', {
+        url: '/map',
         views: {
-          'tab-map': {
-            templateUrl: 'main/templates/mappy.html',
-            controller: 'MappyCtrl as mappyCtrl'
-          }
+            'tab-map': {
+                templateUrl: 'main/templates/map.html',
+                controller: 'MapCtrl as mapCtrl'
+            }
         }
-      })
-      .state('main.list', {
-        url: '/list',
-        views: {
-          'tab-list': {
-            templateUrl: 'main/templates/list2.html',
-            controller: 'ListyCtrl as listyCtrl'
-          }
-        }
-      })
-      // .state('main.mappy', {
-      //   url: '/mappy',
-      //   controller: 'MappyCtrl as mappyCtrl',
-      //   views: {
-      //     'tab-mappy': {
-      //       templateUrl: 'main/templates/mappy.html'
-
-      //     },
-      //     'tab-list': {
-      //       templateUrl: 'main/templates/list2.html'
-      //       // controller: 'MappyCtrl as mappyCtrl'
-      //     }
-      //   }
-      // })
-      .state('main.list-detail', {
+    })
+    .state('main.list-detail', {
         url: 'mappydetail/:objectId',
         views: {
-          'tab-mappy': {
-            templateUrl: 'main/templates/list-detail.html',
-            controller: 'TestShowCtrl as ctrl',
-            resolve: {
-              objectId: function($stateParams) {
-                return $stateParams.objectId;
-              }
+            'tab-mappy': {
+                templateUrl: 'main/templates/list-detail.html',
+                controller: 'TestShowCtrl as ctrl',
+                resolve: {
+                    objectId: function($stateParams) {
+                        return $stateParams.objectId;
+                    }
+                }
             }
-          }
         }
-      })
-      // .state('main.opinionate', {
-      //   url: '/opinionate',
-      //   views: {
-      //     'tab-opinionate': {
-      //       templateUrl: 'main/templates/opinionator.html',
-      //       controller: 'OpinionatorCtrl'
-      //     }
-      //   }
-      // })
-      ;
+    });
 });
