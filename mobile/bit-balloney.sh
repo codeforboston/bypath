d4af08e1b0745dfe28933a73ee885f740921484c
Andrew Lawson <aeriklawson@gmail.com>
2016-05-27 13:19:14 -0400

    Remove privateryan folder. Remove old mobile folder.

--- mobile/bit-balloney.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rsync -avz -L --progress app/ ~/Desktop/$1 # not www/
-rsync -avz -L --progress .tmp/main/styles/main.css ~/Desktop/$1/main/styles/
-rm -r ~/Desktop/$1/main/styles/*.scss
-
--- privateryan/www/main/templates/filterer.html
+++
@@ -1,1 +0,0 @@
-<h1>asdfasdf</h1>
