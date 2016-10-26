e2b77377138ab2f55dc2595c0b95f90e0fbb1cdf
Josh <jdlazar2011@gmail.com>
2016-10-25 21:51:36 -0400

    gMaps version with promise plugin errors

--- v2/app/pages/mapHome/mapHome.ts
+++ v2/app/pages/mapHome/mapHome.ts
@@ -1,18 +1,46 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {Component} from '@angular/core'
+import {NavController, Platform} from 'ionic-angular'
+import { GoogleMap, GoogleMapsEvent, GoogleMapsLatLng } from 'ionic-native'
 
 @Component({
   templateUrl: 'build/pages/mapHome/mapHome.html'
 })
 export class mapHome {
 
-  constructor(private navCtrl: NavController) {}
-
-  ionViewDidLoad() {
-    console.log('Hello Users Page')
-    return (
-      let myMap =  L.map('mapId').setView([51.505, -0.09], 13)
-    )
+   map: GoogleMap;
+   constructor(private navCtrl: NavController, public platform: Platform ) {
+     platform.ready().then(() => {
+            this.loadMap()
+        })
   }
+  loadMap(){
 
+        let location = new GoogleMapsLatLng(-34.9290,138.6010);
+
+        this.map = new GoogleMap('map', {
+          'backgroundColor': 'white',
+          'controls': {
+            'compass': true,
+            'myLocationButton': true,
+            'indoorPicker': true,
+            'zoom': true
+          },
+          'gestures': {
+            'scroll': true,
+            'tilt': true,
+            'rotate': true,
+            'zoom': true
+          },
+          'camera': {
+            'latLng': location,
+            'tilt': 30,
+            'zoom': 15,
+            'bearing': 50
+          }
+        })
+
+        this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+            console.log('Map is ready!')
+        })
+    }
 }
