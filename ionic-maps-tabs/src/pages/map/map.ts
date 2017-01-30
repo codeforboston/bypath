4071e2b98b3a2baa5c1a6965cc5a6e9b6bf21d47
Josh <jdlazar2011@gmail.com>
2017-01-30 15:19:12 -0500

    updates w map objs pulling from db

--- ionic-maps-tabs/src/pages/map/map.ts
+++ ionic-maps-tabs/src/pages/map/map.ts
@@ -4,6 +4,100 @@ import { NavController } from 'ionic-angular'
 import { Geolocation } from 'ionic-native'
 import 'rxjs/add/operator/map'
 
+declare var google
+
+@Component({
+  selector: 'home-page',
+  templateUrl: 'map.html'
+})
+
+export class MapPage {
+
+  @ViewChild('map') mapElement: ElementRef
+
+  map: any
+  mapPoints: any
+
+  constructor(public navCtrl: NavController, public http: Http) {
+
+    //this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').map(res => res.json()).subscribe(
+    this.http.get('http://localhost:8080/incident/get?x=42&y=-71&d=1').map(res => res.json()).subscribe(
+    //this.http.get('http://bypath.herokuapp.com/incident/get?x=42&y=-71&d=1').map(res => res.json()).subscribe(
+      data => {
+        this.mapPoints = data
+        console.log(this.mapPoints)
+      },
+      err => {
+        console.log('error!!! :)')
+      }
+    )
+  }
+
+
+
+
+
+  ionViewDidLoad()  {
+    console.log("got to zero")
+    this.loadMap()
+  }
+
+
+
+
+
+  loadMap(){
+    Geolocation.getCurrentPosition().then((position) => {
+
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+
+      let mapOptions = {
+        zoomControl: true,
+        zoomControlOptions: {
+          style: google.maps.ZoomControlStyle.SMALL,
+          position: google.maps.ControlPosition.RIGHT
+        },
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+
+      let map = new google.maps.Map(this.mapElement.nativeElement, mapOptions)
+      console.log("Got to 1")
+
+      this.mapPoints.forEach(function(point) {
+        let latLong = new google.maps.LatLng(point.latitude, point.longitude)
+        let marker = new google.maps.Marker({
+          position: latLong,
+          map: map
+        })
+        /*
+        let content = "<h4>point.title  </h4>"
+        this.addInfoWindow(marker, content)
+
+        addInfoWindow(marker, content) {
+          let infoWindow = new google.maps.InfoWindow({
+            content: content
+          })
+          google.maps.event.addListener(marker, 'click', () => {
+            infoWindow.open(this.map, marker)
+          })
+        }
+        */
+      })
+
+    }), (err) => {
+      console.log(err)
+     }
+  }
+  //console.log("got to 2 ")
+}
+
+
+
+
+
+
 /*
 const mapPoints = this.http.get('localhost:3000/incident/get?x=42&y=-71&d=1').map(res => res.json()).subscribe(data => {
    console.log(data)
@@ -51,86 +145,10 @@ const mapPoints = [
 ]
 */
 
-declare var google
-
-@Component({
-  selector: 'home-page',
-  templateUrl: 'map.html'
+/*
+let marker = new google.maps.Marker({
+  position: latLng,
+  map: this.map,
+  title: 'Hello World!'
 })
-
-export class MapPage {
-
-  @ViewChild('map') mapElement: ElementRef
-
-  map: any
-  mapPoints: any
-
-  constructor(public navCtrl: NavController, public http: Http) {
-
-    //this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').map(res => res.json()).subscribe(
-    this.http.get('http://localhost:8080/incident/get?x=42&y=-71&d=1').map(res => res.json()).subscribe(
-    //this.http.get('http://bypath.herokuapp.com/incident/get?x=42&y=-71&d=1').map(res => res.json()).subscribe(
-      data => {
-        this.mapPoints = data
-        console.log(this.mapPoints)
-      },
-      err => {
-        console.log('error!!! :)')
-      }
-    )
-  }
-
-  ionViewDidLoad()  {
-    console.log("got to zero")
-    this.loadMap()
-  }
-
-  loadMap(){
-
-    Geolocation.getCurrentPosition().then((position) => {
-
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
-
-      let mapOptions = {
-        zoomControl: true,
-        zoomControlOptions: {
-          style: google.maps.ZoomControlStyle.SMALL,
-          position: google.maps.ControlPosition.RIGHT
-        },
-        center: latLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-
-      let map = new google.maps.Map(this.mapElement.nativeElement, mapOptions)
-      console.log("Got to 1")
-
-
-      /*
-      let marker = new google.maps.Marker({
-        position: latLng,
-        map: this.map,
-        title: 'Hello World!'
-      })
-      */
-
-
-    this.mapPoints.forEach(function(point) {
-
-        let latLong = new google.maps.LatLng(point.latitude, point.longitude)
-
-        let marker = new google.maps.Marker({
-          position: latLong,
-          map: map,
-          // icon: icon,
-          title: point.title
-        })
-      })
-    }, (err) => {
-      console.log(err)
-    })
-
-    console.log("got to 2 ")
-
-  }
-}
+*/
